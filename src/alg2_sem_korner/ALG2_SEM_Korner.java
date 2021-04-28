789159b638c2fcdaab197cd7a49a3f8f90e83720
elPytel <jaroslav.korner1@gmail.com>
2021-04-28 13:53:22 +0200

    java init

---
+++ src/alg2_sem_korner/ALG2_SEM_Korner.java
@@ -0,0 +1,16 @@
+package alg2_sem_korner;
+
+/**
+ *
+ * @author pytel
+ */
+public class ALG2_SEM_Korner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
