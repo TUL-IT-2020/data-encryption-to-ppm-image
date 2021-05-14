c20b1c4c8f043933e9e3777bb5f24133ee934727
elPytel <jaroslav.korner1@gmail.com>
2021-05-14 20:03:42 +0200

    getNextNbites

--- src/Logic/DataFile.java
+++ src/Logic/DataFile.java
@@ -15,6 +15,7 @@ public class DataFile {
     private String name;
     private String format;
     
+    public static int BYTE_LENGHT = 8;
     private int ByteIndex;
     private int BitIndex;
     private byte[] FileContent = null;
@@ -55,14 +56,47 @@ public class DataFile {
      * Load file content to memory.
      */
     void ReadFile() throws FileNotFoundException, IOException {
+        // reset pointers
+        BitIndex = 0;
+        ByteIndex = 0;
+        
         // TODO change, nead lot of memory!!!
         // TODO asi to shoří na velký soubory: long -> int
         
-        // TODO try with resorcis
         FileContent = new byte[(int)getFileSize()];
-        FileInputStream fis = new FileInputStream(file);
-        fis.read(FileContent);
-        fis.close();
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(FileContent);
+        }
+    }
+
+    private byte nthBitFromLeft(byte B, int index) {
+        //System.out.format("Shift: %s \t last: %s\n", Integer.toString(B >> 7-index,2), Integer.toString((B >> 7-index) & 1,2));
+        return (byte)((B >> 7-index) & 1);
+    }
+    
+    private char byte2char (byte B) {
+        return (char)(B & 0xFF);
+    }
+    
+    public Byte getNextNbites(int chunkSize) {
+        if (ByteIndex >= FileContent.length) return null;
+        byte bitArray = 0;
+        byte nthBit = 0;
+        byte B;
+        for (int i = 0; i < chunkSize; i++) {
+            // TODO max index cap !!!
+            if (BitIndex >= BYTE_LENGHT) {
+                ByteIndex += 1;
+                BitIndex = 0;
+            }
+            B = FileContent[ByteIndex];
+            nthBit = (byte) (nthBitFromLeft(B, BitIndex) << chunkSize-i-1);
+            bitArray = (byte) (bitArray | nthBit);
+            //System.out.format("Index Byte: %d content: %s \t bit: %d content: %s \n", ByteIndex, byte2char(B), BitIndex, Integer.toString(nthBit & 0xFF,2));
+            //System.out.format("Byte: %s\n", Integer.toString(bitArray & 0xFF,2));
+            BitIndex += 1;
+        }
+        return bitArray;
     }
 
     
--- test/Logic/DataFileTest.java
+++ test/Logic/DataFileTest.java
@@ -13,16 +13,16 @@ import static org.junit.Assert.*;
  * @author pytel
  */
 public class DataFileTest {
-    
+
     public static File path = new File(System.getProperty("user.dir") + "/Data/testDataSet");
     public static String name = "test";
     public static String format = ".txt";
     public static File filePath = new File(path, name + format);
     public static long size = 6;
-    
+
     public DataFileTest() {
     }
-    
+
     public DataFile loadFile(File filePath) {
         DataFile df = new DataFile();
         try {
@@ -38,22 +38,22 @@ public class DataFileTest {
         DataFile df = loadFile(filePath);
         assertNotNull(df);
     }
-    
+
     @Test
     public void createDataFileAndGetName() {
         DataFile df = loadFile(filePath);
-        assert df.getName().compareTo(name) == 0 : "Invalid name: " + 
-                df.getName() + " != " + name;
+        assert df.getName().compareTo(name) == 0 : "Invalid name: "
+                + df.getName() + " != " + name;
     }
-    
+
     @Test
     public void GetFileSize() {
         DataFile df = loadFile(filePath);
         //System.out.format("File Size: %d\n", df.getFileSize());
-        assert df.getFileSize() == size : "Invalid size: " + 
-                df.getFileSize() + " != " + size;
+        assert df.getFileSize() == size : "Invalid size: "
+                + df.getFileSize() + " != " + size;
     }
-    
+
     @Test
     public void TestReadFile() {
         DataFile df = loadFile(filePath);
@@ -63,18 +63,21 @@ public class DataFileTest {
             System.out.format("ERROR: %s\n", ex);
         }
     }
-    
-    /*
+
     @Test
     public void TestGetNBites() {
         DataFile df = loadFile(filePath);
-        int chunk = 4;
-        df.ReadFile();
-        for (int i = 0; i < df.getFileSize()*8; i++) {
-            System.out.format("File Size: %d\n", df.getNextNbites(chunk));
+        int chunk = 8;
+        try {
+            df.ReadFile();
+            for (int i = 0; i < df.getFileSize()*8/chunk; i++) {
+                System.out.format("Chunk content: %s\n", (char)(df.getNextNbites(chunk) & 0xFF));
+            }
+            //assert df.getFileSize() == size : "Invalid size: " +
+            //        df.getFileSize() + " != " + size;
+        } catch (IOException ex) {
+            System.out.format("ERROR: %s\n", ex);
         }
-        //assert df.getFileSize() == size : "Invalid size: " + 
-        //        df.getFileSize() + " != " + size;
-    }*/
-    
+    }
+
 }
